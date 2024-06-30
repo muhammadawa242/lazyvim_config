acb14c0048d1c66a3e854ea6acb9bacb4cdd27d1
Awais <muhammadawa242@gmail.com>
2024-06-30 18:12:29 +0500

    python formatter and undo tree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,5 +7,12 @@ vim.keymap.set("n", "<A-k>", ":m .-2<CR>") -- move line down(n)
 vim.keymap.set("n", "<F8>", ":TagbarToggle<CR>") -- f8 to open TagbarToggle
 vim.keymap.set("n", "zz", "<cmd>GlobalNote<cr>") -- for global note
 vim.keymap.set("n", "<A-t>", "<cmd>TransparentToggle<cr>") -- for global note
-vim.keymap.set("n", "<C-A-c>", "<cmd>CellularAutomaton make_it_rain<cr>")
-vim.keymap.set("n", "<C-A-x>", "<cmd>CellularAutomaton game_of_life<cr>")
+-- vim.keymap.set("n", "<C-c>", "<cmd>CellularAutomaton make_it_rain<cr>")
+-- vim.keymap.set("n", "<C-x>", "<cmd>CellularAutomaton game_of_life<cr>")
+vim.keymap.set("i", "<A-x>", "<Esc>dbxi")
+vim.keymap.set("i", "<A-u>", "<Esc>uwi")
+vim.keymap.set("n", "<A-l>", "<cmd>Gitsigns next_hunk<cr>")
+vim.keymap.set("n", "<A-h>", "<cmd>Gitsigns prev_hunk<cr>")
+vim.keymap.set("n", "<F5>", "<cmd>UndotreeToggle<cr>")
+-- vim.keymap.set("n", "<C-b>", "<cmd>Gitsigns blame_line<cr>")
+-- vim.keymap.set("n", "<C-k>", "<cmd>Gitsigns preview_hunk<cr>")
