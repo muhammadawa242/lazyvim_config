ba101a936d380ef4e6a4bd12eee270da4d3cab98
muhammadawa242 <muhammadawa242@gmail.com>
2024-08-17 03:49:29 +0500

    yes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+    callback = function()
+        vim.cmd("sleep 10m")
+    end,
+})
+
+-- vim.api.nvim_create_autocmd({ "VimLeave" }, {
+--     callback = function()
+--         vim.fn.jobstart('notify-send "hello"', { detach = true })
+--     end,
+-- })
